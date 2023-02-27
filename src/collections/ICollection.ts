f58acf4aa7ce92337c41cd754f23e5f6cd1ca5fd
Yobuligo <hoffmannpeter82@gmail.com>
2023-02-27 11:24:56 +0100

    Add documentation

--- src/collections/ICollection.ts
+++ src/collections/ICollection.ts
@@ -1,3 +1,4 @@
+import { NoSuchElementException } from "@yobuligo/core.typescript";
 import { IList } from "./../lists/IList";
 import { ICollectionExtension } from "./ICollectionExtension";
 
@@ -6,18 +7,58 @@ import { ICollectionExtension } from "./ICollectionExtension";
  */
 export interface ICollection<T> extends ICollectionExtension<T> {
   /**
-   * Returns the number of elements within that list.
+   * Returns the number of elements in that list.
    */
   readonly size: number;
+
+  /**
+   * Returns the index of the last element in that list.
+   */
   readonly lastIndex: number;
+
+  /**
+   * Returns *true* if the passed in *{@link element}* is part of the list. Otherwise it returns false.
+   */
   contains(element: T): boolean;
+
+  /**
+   * Returns *true* if all passed in *{@link elements}* are part of the list. Otherwise it returns false.
+   */
   containsAll(...elements: T[]): boolean;
+
+  /**
+   * Returns *true* if the passed in *{@link element}* is not part of the list. Otherwise it returns true.
+   */
   containsNot(element: T): boolean;
+
+  /**
+   * Returns a new created list, which contains all elements of the origin list beside duplicates, no matter if *{@link T}* is a scalar type or a reference type.
+   */
   distinct(): IList<T>;
+
+  /**
+   * Returns a new created list, which contains all elements of the origin list beside duplicates. Duplicates are identified by comparing the property key of *{@link T}*, which is returned by the *{@link selector}*.
+   */
   distinctBy<K extends keyof T>(selector: () => K): IList<T>;
+
+  /**
+   * Returns the element at position *{@link index}* or throws a *{@link NoSuchElementException}* exception if *{@link index}* is not assigned.
+   */
   elementAt(index: number): T;
+
+  /**
+   * Returns the element at position *{@link index}* or returns *undefined* if *{@link index}* is not assigned.
+   */
   elementAtOrNull(index: number): T | undefined;
+
+  /**
+   * Returns a new created list, which contains all elements who match the filter provided by function *{@link block}*. If no element matches the filter an empty list is returned.
+   */
   filter(block: (element: T) => boolean): IList<T>;
+
+  /**
+   * Returns the first element, which matches the filter provided by function *{@link block}*. If no element matches the filter *undefined* is returned.
+   */
   find(block: (element: T) => boolean): T | undefined;
   first(): T;
   firstOrNull(): T | undefined;
